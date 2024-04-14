0bf3b23554d40e6511209079a843fcd766b3d09a
Matthew Wicks <mwicks7@gmail.com>
2024-04-14 15:35:22 -0400

    Optimize images

--- components/parkCard.js
+++ components/parkCard.js
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
 
 export default function ParkCard({ park }) {
   return (
@@ -7,23 +8,32 @@ export default function ParkCard({ park }) {
         <Link href={`/${park.parkCode}/park`}>
           <h2 className="park-card__name">{park.name}</h2>
         </Link>
-        <p className="park-card__location">{park.location.city}, {park.location.state}</p>
+        <p className="park-card__location">
+          {park.location.city}, {park.location.state}
+        </p>
       </div>
 
       <Link href={`/${park.parkCode}/park`}>
-        <div className="park-card__image" style={{backgroundImage: `url(/images/${park.parkCode}.jpg)`}}>
-          <span className="sr-only">More info on {park.name}</span>
+        <div className="park-card__image">
+          <Image
+            src={`/images/${park.parkCode}.jpg`}
+            height={300}
+            width={460}
+            quality={100}
+            loading="lazy"
+            sizes="460px"
+            alt=""
+            unoptimized
+          />
         </div>
       </Link>
 
       <div className="park-card__details">
         <p className="park-card__description">{park.description}</p>
         <div className="park-card__links">
-          <Link href={`/${park.parkCode}/park`}>
-            More info on {park.name}
-          </Link>
+          <Link href={`/${park.parkCode}/park`}>More info on {park.name}</Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
