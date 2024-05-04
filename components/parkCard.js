97efeacc84b39c016289900f2238e6aecbe66d81
Matthew Wicks <mwicks7@gmail.com>
2024-05-04 16:20:00 -0400

    Address lighthouse suggestions

--- components/parkCard.js
+++ components/parkCard.js
@@ -1,37 +1,35 @@
 import Link from "next/link"
 import Image from "next/image"
 
-export default function ParkCard({ park }) {
+export default function ParkCard({ park, imgLoading = "lazy" }) {
   return (
     <div key={park.parkCode} className="park-card">
       <div className="park-card__header">
-        <Link href={`/${park.parkCode}/park`}>
-          <h3 className="park-card__name">{park.name}</h3>
-        </Link>
+        <h3 className="park-card__name">
+          <Link className="park-card__link" href={`/${park.parkCode}/park`}>
+            {park.name}
+          </Link>
+        </h3>
         <p className="park-card__location">
           {park.location.city}, {park.location.state}
         </p>
       </div>
 
-      <Link href={`/${park.parkCode}/park`}>
-        <div className="park-card__image">
-          <Image
-            src={`/images/original/${park.parkCode}.jpg`}
-            height={300}
-            width={900}
-            quality={100}
-            loading="lazy"
-            sizes="900px"
-            alt=""
-          />
-        </div>
-      </Link>
+      <div className="park-card__image">
+        <Image
+          src={`/images/original/${park.parkCode}.jpg`}
+          height={300}
+          width={900}
+          quality={100}
+          loading={imgLoading}
+          priority={imgLoading === "eager"}
+          sizes="900px"
+          alt=""
+        />
+      </div>
 
       <div className="park-card__details">
         <p className="park-card__description">{park.description}</p>
-        <div className="park-card__links">
-          <Link href={`/${park.parkCode}/park`}>More info on {park.name}</Link>
-        </div>
       </div>
     </div>
   )
