bce43210fdc562750ca3b1f07ac4839e5c21b13d
Matthew Wicks <mwicks7@gmail.com>
2024-05-11 10:58:18 -0400

    Remove photos page and update articles to use new component

--- pages/[id]/articles.js
+++ pages/[id]/articles.js
@@ -1,48 +1,26 @@
-import { getParkPaths, getParkInfo, getParkData } from '../../lib/dbParks'
-import Layout from '../../components/layout'
-import ParkPage from '../../components/parkPage'
-import MediaCard from '../../components/mediaCard'
+import { getParkPaths, getParkInfo, getParkData } from "../../lib/dbParks"
+import ParkPage from "../../components/parkPage"
 
 export async function getStaticPaths() {
   const paths = await getParkPaths()
   return {
     paths,
     fallback: false,
-  };
+  }
 }
 
 export async function getStaticProps({ params }) {
   const parkInfo = await getParkInfo(params.id)
-  const parkData = await getParkData('articles', params.id)
+  const parkData = await getParkData("articles", params.id)
 
   return {
     props: {
       parkInfo: parkInfo,
       data: parkData.data,
-    }
+    },
   }
 }
 
 export default function Articles({ parkInfo, data }) {
-  return (
-    <Layout>
-      <ParkPage
-        pageTitle='Articles'
-        parkInfo={parkInfo}
-      >
-        {data.map((article) => (
-          <MediaCard
-            imgHeight={500}
-            title={article.title}
-            subtitle={article.description}
-            description={article.listingDescription}
-            img={article.listingImage.url}
-            links={[
-              {href: article.url, text: 'Read more at nps.gov'}
-            ]}
-          />
-        ))}
-      </ParkPage>
-    </Layout>
-  )
+  return <ParkPage pageTitle="Articles" parkInfo={parkInfo} data={data} />
 }
